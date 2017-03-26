f8d2105c0fbb370301a6e89e4b3df2fe6fd39975
serhiipianykh <serhiipianykh@gmail.com>
2017-03-26 18:05:55 -0400

    bulb takes state from cloud

--- app/src/main/java/com/socioty/smartik/LedSmartLightActivity.java
+++ app/src/main/java/com/socioty/smartik/LedSmartLightActivity.java
@@ -1,21 +1,20 @@
 package com.socioty.smartik;
 
 import android.graphics.Color;
-import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.ColorInt;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
-import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.enrico.colorpicker.colorDialog;
 import com.triggertrap.seekarc.SeekArc;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.List;
@@ -31,6 +30,7 @@ import cloud.artik.model.Action;
 import cloud.artik.model.ActionArray;
 import cloud.artik.model.Actions;
 import cloud.artik.model.MessageIDEnvelope;
+import cloud.artik.model.NormalizedMessagesEnvelope;
 
 public class LedSmartLightActivity extends AppCompatActivity implements colorDialog.ColorSelectedListener {
 
@@ -43,14 +43,20 @@ public class LedSmartLightActivity extends AppCompatActivity implements colorDia
 
     private ImageButton imageButton;
     private TextView state;
+
     private boolean isOn = false;
+    private int intensity = 0;
+    private int rColor = 0;
+    private int gColor = 0;
+    private int bColor = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.led_smart_light_main);
 
-
+        initializeMessagesApi(Token.sToken.getToken());
+        this.deviceId = getIntent().getStringExtra(KEY_DEVICE_ID);
         imageButton = (ImageButton) findViewById(R.id.switcher);
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,29 +69,26 @@ public class LedSmartLightActivity extends AppCompatActivity implements colorDia
         });
 
 
-        enableComponentsBasedOnState(false);
-        this.deviceId = getIntent().getStringExtra(KEY_DEVICE_ID);
-        initializeMessagesApi(Token.sToken.getToken());
+        getLatestMsg();
 
         state = (TextView) findViewById(R.id.lightIndicatorText);
 
-
-        configureColorButton();
-        configureIntensityPicker();
     }
 
     private void enableComponentsBasedOnState(final boolean state) {
         final View colorView = (View) findViewById(R.id.ledColorView);
         final SeekArc intensitySeekArc = (SeekArc) findViewById(R.id.ledIntensitySeekArc);
-
         colorView.setEnabled(state);
+        changeImage(imageButton, isOn);
         intensitySeekArc.setEnabled(state);
     }
 
     private void configureColorButton() {
         final View colorView = (View) findViewById(R.id.ledColorView);
-        colorDialog.setPickerColor(LedSmartLightActivity.this, 1, Color.YELLOW);
-
+        int rgb = rColor;
+        rgb = (rgb << 8) + gColor;
+        rgb = (rgb << 8) + bColor;
+        colorDialog.setPickerColor(LedSmartLightActivity.this, 1, rgb);
         colorView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View view) {
@@ -97,7 +100,8 @@ public class LedSmartLightActivity extends AppCompatActivity implements colorDia
     private void configureIntensityPicker() {
         final SeekArc intensitySeekArc = (SeekArc) findViewById(R.id.ledIntensitySeekArc);
         final TextView seekArcProgress = (TextView) findViewById(R.id.seekArcProgress);
-
+        intensitySeekArc.setProgress(intensity);
+        seekArcProgress.setText(String.valueOf(intensity));
         intensitySeekArc.setOnSeekArcChangeListener(new SeekArc.OnSeekArcChangeListener() {
             int latestProgress = 0;
             @Override
@@ -222,5 +226,71 @@ public class LedSmartLightActivity extends AppCompatActivity implements colorDia
             imageButton.setImageResource(R.mipmap.off_button);
             state.setText(getString(R.string.light_off));
         }
+    }
+
+    private void getLatestMsg() {
+        final String tag = "Bulb getLastNormalizedMessagesAsync";
+        try {
+            int messageCount = 1;
+            messagesApi.getLastNormalizedMessagesAsync(messageCount, deviceId, null,
+                    new ApiCallback<NormalizedMessagesEnvelope>() {
+                        @Override
+                        public void onFailure(ApiException exc, int i, Map<String, List<String>> stringListMap) {
+                            processFailure(tag, exc);
+                        }
+
+                        @Override
+                        public void onSuccess(NormalizedMessagesEnvelope result, int i, Map<String, List<String>> stringListMap) {
+                            Log.v(tag, " onSuccess latestMessage = " + result.getData().toString());
+                            String mid = "";
+                            String data = "";
+                            if (!result.getData().isEmpty()) {
+                                mid = result.getData().get(0).getMid();
+                                data = result.getData().get(0).getData().toString();
+                            }
+                            try {
+                                JSONObject json = new JSONObject(data);
+                                isOn = json.getBoolean("state");
+                                intensity = json.getInt("intensity");
+                                JSONObject colors = json.getJSONObject("colorRGB");
+                                rColor = colors.getInt("r");
+                                gColor = colors.getInt("g");
+                                bColor = colors.getInt("b");
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    initUI();
+                                }
+                            });
+
+                        }
+
+                        @Override
+                        public void onUploadProgress(long bytes, long contentLen, boolean done) {
+                        }
+
+                        @Override
+                        public void onDownloadProgress(long bytes, long contentLen, boolean done) {
+                        }
+                    });
+        } catch (ApiException exc) {
+            processFailure(tag, exc);
+        }
+    }
+
+    private void processFailure(final String context, ApiException exc) {
+        String errorDetail = " onFailure with exception" + exc;
+        Log.w(context, errorDetail);
+        exc.printStackTrace();
+    }
+
+    private void initUI() {
+        configureColorButton();
+        configureIntensityPicker();
+        enableComponentsBasedOnState(isOn);
     }
 }
