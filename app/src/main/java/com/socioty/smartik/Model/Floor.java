6184e7720cae98b7e6f1b904e27892a91e870b04
Willian Campos <willian.campos@gmail.com>
2017-04-18 14:16:37 -0400

    Add model classes

---
+++ app/src/main/java/com/socioty/smartik/Model/Floor.java
@@ -0,0 +1,27 @@
+package com.socioty.smartik.model;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Floor {
+
+	private Set<Room> rooms;
+
+	protected Floor() {
+	}
+
+	public Floor(final Set<Room> rooms) {
+		this.rooms = new HashSet<>(rooms);
+	}
+	
+	public Set<Room> getRooms() {
+		return Collections.unmodifiableSet(rooms);
+	}	
+
+	@Override
+	public String toString() {
+		final StringBuilder builder = new StringBuilder();
+		return builder.append("rooms: ").append(rooms).toString();
+	}
+}
---
+++ app/src/main/java/com/socioty/smartik/Model/Room.java
@@ -0,0 +1,43 @@
+package com.socioty.smartik.model;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Room {
+
+	private String name;
+	private Set<String> deviceIds;
+
+	protected Room() {
+	}
+
+	public Room(final String name, final Set<String> deviceIds) {
+		this.name = name;
+		this.deviceIds = new HashSet<>(deviceIds);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Set<String> getDeviceIds() {
+		return Collections.unmodifiableSet(deviceIds);
+	}
+	
+	public void addDevice(final String deviceId) {
+		this.deviceIds.add(deviceId);
+	}
+	
+	public boolean removeDevice(final String deviceId) {
+		return this.deviceIds.remove(deviceId);
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder builder = new StringBuilder();
+		return builder
+				.append("name: ").append(name).append(", ")
+				.append("deviceIds: ").append(deviceIds).append(", ").toString();
+	}
+}
